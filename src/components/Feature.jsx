b65329f5682532cf752cad8ad6936c6de5a64ca0
luchian <carstoiu.luchian@gmail.com>
2026-04-02 10:25:56 +0300

    new link functionality

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,11 +1,18 @@
 import { storyblokEditable } from '@storyblok/react/rsc';
+import Link from 'next/link';
 
 const Feature = ({ blok }) => {
-	return (
+	const content = (
 		<div className="feature" {...storyblokEditable(blok)}>
 			<span>{blok.name}</span>
 		</div>
 	);
+
+	if (blok.link) {
+		return <Link href={blok.link}>{content}</Link>;
+	}
+
+	return content;
 };
 
 export default Feature;
